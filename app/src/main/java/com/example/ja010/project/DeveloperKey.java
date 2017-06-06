ce4ae69617dda89e9d26fa3f8a32c2adedb8ceb9
ja01001 <ja01001@naver.com>
2017-06-06 18:23:07 +0900

    youtube 연동및 layout 수정

---
+++ app/src/main/java/com/example/ja010/project/DeveloperKey.java
@@ -0,0 +1,12 @@
+package com.example.ja010.project;
+
+/**
+ * Created by ja010 on 17-06-06.
+ */
+
+public class DeveloperKey {
+
+
+
+    public static final String DEVELOPER_KEY = "AIzaSyDJ2Kh_JrPb-5I5Oo0ALPPlDb-0eT-UJMY";
+}
--- app/src/main/java/com/example/ja010/project/MainActivity.java
+++ app/src/main/java/com/example/ja010/project/MainActivity.java
@@ -8,14 +8,24 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Switch;
-import android.widget.VideoView;
+import android.widget.Toast;
 
+/**
+ * A simple YouTube Android API demo application which shows how to create a simple application that
+ * displays a YouTube Video in a {@link YouTubePlayerView}.
+ * <p>
+ * Note, to use a {@link YouTubePlayerView}, your activity must extend {@link YouTubeBaseActivity}.
+ */
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends  YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
     Button BNT_STORE,BNT_RANDOM,BNT_DOWNLOAD,BNT_SEARCH;
-    VideoView video;
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
     EditText ED_SEARCH;
     ListView lv;
     ArrayList<dataclass > list = new ArrayList<dataclass>();
@@ -25,18 +35,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ED_SEARCH = (EditText)findViewById(R.id.ed);
-        video = (VideoView)findViewById(R.id.vidio);
-        list.add(new dataclass("asda","asdads"));
+        lv = (ListView)findViewById(R.id.listview);
         adapter = new DataAdapter(this,list);
         lv.setAdapter(adapter);
-
+        list.add(new dataclass("naver","aaa"));
+        list.add(new dataclass("11111","112323"));
+        adapter.notifyDataSetChanged();
+        YouTubePlayerView youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        youTubeView.initialize(DeveloperKey.DEVELOPER_KEY, this);
 
 
     }
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+                                        boolean wasRestored) {
+        if (!wasRestored) {
+            player.cueVideo("wKJ9KzGQq0w");
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(getString(R.string.error_player), youTubeInitializationResult.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(DeveloperKey.DEVELOPER_KEY, this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return (YouTubePlayerView) findViewById(R.id.youtube_view);
+    }
+
     public void clcl(View v){
         switch(v.getId()){
             case R.id.btnsearch:
-
+                lv.deferNotifyDataSetChanged();
                 break;
             case R.id.btnstore:
 
