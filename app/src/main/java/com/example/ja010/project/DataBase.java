179fd5296fa9e689870127440c78e392065b1954
ja01001 <ja01001@naver.com>
2017-06-09 20:06:53 +0900

    apache 를 이용해 처음 목록의 id를 불러옴

---
+++ app/src/main/java/com/example/ja010/project/DataBase.java
@@ -0,0 +1,8 @@
+package com.example.ja010.project;
+
+/**
+ * Created by ja010 on 17-06-08.
+ */
+
+public class DataBase {
+}
--- app/src/main/java/com/example/ja010/project/MainActivity.java
+++ app/src/main/java/com/example/ja010/project/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ja010.project;
 
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,6 +24,20 @@ import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 public class MainActivity extends  YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
@@ -28,6 +45,9 @@ public class MainActivity extends  YouTubeBaseActivity implements YouTubePlayer.
     private static final int RECOVERY_DIALOG_REQUEST = 1;
     EditText ED_SEARCH;
     ListView lv;
+    String vodid = "";
+
+    Task t;
     ArrayList<dataclass > list = new ArrayList<dataclass>();
     DataAdapter adapter;
     @Override
@@ -80,24 +100,92 @@ public class MainActivity extends  YouTubeBaseActivity implements YouTubePlayer.
     public void clcl(View v){
         switch(v.getId()){
             case R.id.btnsearch:
-                lv.deferNotifyDataSetChanged();
-                break;
-            case R.id.btnstore:
+                t = new Task();
+                t.execute();
 
+
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.btnstore: // db에 저장
                 break;
             case R.id.btnrandom:
-
                 break;
             case R.id.btndownload:
                 Intent i = new Intent(MainActivity.this,Internet.class);
                 startActivity(i);
-                finish();
+
 
                 break;
 
         }
 
     }
-    // internet setting
+    public JSONObject getUtube(){
+        HttpGet httpGet = new HttpGet(
+                "https://www.googleapis.com/youtube/v3/search?"
+                        + "part=snippet&q=" +ED_SEARCH.getText().toString()
+                        + "&key="+DeveloperKey.DEVELOPER_KEY+"&maxResults=50");
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response;
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            response = client.execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            InputStream stream = entity.getContent();
+            int b;
+            while ((b = stream.read()) != -1) {
+                stringBuilder.append((char) b);
+            }
+        } catch (ClientProtocolException e) {
+        } catch (IOException e) {
+        }
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject = new JSONObject(stringBuilder.toString());
+        } catch (JSONException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+    private void paringJsonData(JSONObject jsonObject) throws JSONException {
+        JSONArray contacts = jsonObject.getJSONArray("items");
+            JSONObject c = contacts.getJSONObject(1);
+            String kind =  c.getJSONObject("id").getString("kind"); // 종류를 체크하여 playlist도 저장
+            if(kind.equals("youtube#video")){
+                vodid = c.getJSONObject("id").getString("videoId"); // 유튜브
+            }else{
+                vodid = c.getJSONObject("id").getString("playlistId"); // 유튜브
+            }
+
+            String title = c.getJSONObject("snippet").getString("title"); //유튜브 제목을 받아옵니다
+        }
+    class Task extends AsyncTask<Void,Void,Void>{
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                paringJsonData(getUtube());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            Toast.makeText(getApplicationContext(),"id: "+vodid,Toast.LENGTH_SHORT).show();
+            super.onPostExecute(aVoid);
+        }
+    }
 
 }
+
+
+    // internet setting
+
